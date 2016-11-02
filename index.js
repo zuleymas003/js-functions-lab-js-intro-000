48abdf16a6e17db1547e580ab053117b5381c753
zuleymas003 <zuleymas27684@students.laalliance.org>
2016-11-02 15:31:21 +0000

    First saved version of my nitrous code.

---
+++ index.js
@@ -0,0 +1,13 @@
+// write your code below!
+var happyHolidays =function(){
+  return "Happy holidays!";
+};
+var happyHolidaysTo = function(){
+  return "Happy holidays, you!";
+};
+var happyHolidayTo =function(){
+  return "Happy Independence Day, you!";
+};
+var holidayCountdown = function(){
+  return "It\'s 20 days until Mother\'s Day!";
+};
